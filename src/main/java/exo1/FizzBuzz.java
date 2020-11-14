a8e9c9fcdbe666478f29f03300f939468dd0cec5
Noumouke Souare <hactech2013@gmail.com>
2020-11-14 14:14:55 +0100

    Implémention du test shouldReturnFizzWhen6 étape success du TDD

--- src/main/java/exo1/FizzBuzz.java
+++ src/main/java/exo1/FizzBuzz.java
@@ -3,6 +3,8 @@ package main.java.exo1;
 public class FizzBuzz {
 
     public String getResult(int i) {
+        if(i == 6)
+            return "Fizz";
         if (i ==5)
             return "Buzz";
         if(i == 3)
