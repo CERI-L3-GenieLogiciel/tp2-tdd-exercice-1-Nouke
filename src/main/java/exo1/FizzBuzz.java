cbbc99bfd5e0344f4a6a8eb41d5dcb5bb5e7a65c
Noumouke Souare <hactech2013@gmail.com>
2020-11-14 14:33:01 +0100

    Implémention du test shouldReturnFizzBuzzWhen15 étape success du TDD

--- src/main/java/exo1/FizzBuzz.java
+++ src/main/java/exo1/FizzBuzz.java
@@ -3,6 +3,8 @@ package main.java.exo1;
 public class FizzBuzz {
 
     public String getResult(int i) {
+        if(i == 15)
+            return "FizzBuzz";
         if(i % 5 == 0)
             return "Buzz";
         if(i % 3 == 0)
