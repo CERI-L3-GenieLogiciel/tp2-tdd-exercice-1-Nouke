a5d26eeeb26e7f51920150746795db449115ac95
Noumouke Souare <hactech2013@gmail.com>
2020-11-14 14:22:19 +0100

    Implémention du test shouldReturnBuzzWhen10 étape success du TDD

--- src/main/java/exo1/FizzBuzz.java
+++ src/main/java/exo1/FizzBuzz.java
@@ -3,6 +3,8 @@ package main.java.exo1;
 public class FizzBuzz {
 
     public String getResult(int i) {
+        if(i == 10)
+            return "Buzz";
         if(i % 3 == 0)
             return "Fizz";
         if (i ==5)
