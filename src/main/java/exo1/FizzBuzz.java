b93a9728709e5ab42772dfd44f0a476fd68b2d3e
Noumouke Souare <hactech2013@gmail.com>
2020-11-14 14:39:08 +0100

    Implémention du test shouldReturnFizzBuzzWhen30 étape refactor du TDD

--- src/main/java/exo1/FizzBuzz.java
+++ src/main/java/exo1/FizzBuzz.java
@@ -3,9 +3,7 @@ package main.java.exo1;
 public class FizzBuzz {
 
     public String getResult(int i) {
-        if(i == 30)
-            return "FizzBuzz";
-        if(i == 15)
+        if(i % 15 == 0)
             return "FizzBuzz";
         if(i % 5 == 0)
             return "Buzz";
